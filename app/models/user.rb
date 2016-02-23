9bfd280c918b573970af23ad8a973acc713ac49a
myu8358 <myu8358@yahoo.co.jp>
2016-02-23 10:18:53 +0000

    amazon_items

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,10 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +34,28 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    have_ownerships.find_or_create_by(item_id: other_item.id)
   end
 
   def unhave(item)
+    have_ownership = have_ownerships.find_by(item_id: item.id)
+    have_ownership.destroy if have_ownership
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    want_ownerships.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    want_ownership = want_ownerships.find_by(item_id: item.id)
+    want_ownership.destroy if want_ownership
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
